dadff52ffd07910f723069d1fd07702679bbd969
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2019-12-02 20:46:55 -0500

    Add a tiny VM

--- 2/Day2/Day2/Program.cs
+++ 2/Day2/Day2/Program.cs
@@ -80,50 +80,13 @@ namespace Day2
 
 		private static List<int> IntCode(List<int> input)
 		{
-			return IntCode(0, input);
-		}
+			Memory memory = new Memory(input);
 
-		private static List<int> IntCode(int index, List<int> input)
-		{
-			bool run = true;
-			while (run)
-			{
-				int opcode = input[index];
+			VM vm = new VM(memory);
 
-				switch (opcode)
-				{
-					case 1:
-						{
-							//add
-							Math(input, index, (x, y) => x + y);
-							break;
-						}
-					case 2:
-						{
-							//mult
-							Math(input, index, (x, y) => x * y);
-							break;
-						}
-					case 99:
-						run = false;
-						break;
-					default:
-						throw new InvalidOperationException();
-				}
+			vm.Run();
 
-				index += 4;
-			}
-
-			return input;
-		}
-
-		private static void Math(List<int> input, int index, Func<int, int, int> func)
-		{
-			int index1 = input[index + 1];
-			int index2 = input[index + 2];
-			int destIndex = input[index + 3];
-
-			input[destIndex] = func(input[index1], input[index2]);
+			return memory.ReadAll();
 		}
 
 		const string Input = @"1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,10,23,2,10,23,27,1,27,6,31,1,13,31,35,1,13,35,39,1,39,10,43,2,43,13,47,1,47,9,51,2,51,13,55,1,5,55,59,2,59,9,63,1,13,63,67,2,13,67,71,1,71,5,75,2,75,13,79,1,79,6,83,1,83,5,87,2,87,6,91,1,5,91,95,1,95,13,99,2,99,6,103,1,5,103,107,1,107,9,111,2,6,111,115,1,5,115,119,1,119,2,123,1,6,123,0,99,2,14,0,0";
---
+++ 2/Day2/Day2/VM.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2
+{
+	public class VM
+	{
+		private readonly Memory _memory;
+
+		public VM(Memory memory)
+		{
+			_memory = memory;
+		}
+
+		public void Run()
+		{
+			int index = 0;
+
+			bool run = true;
+			while (run)
+			{
+				_memory.Set(index, AddressType.OpCode);
+				int opcode = _memory[index];
+
+				switch (opcode)
+				{
+					case 1:
+						{
+							//add
+							Math(index, (x, y) => x + y);
+							break;
+						}
+					case 2:
+						{
+							//mult
+							Math(index, (x, y) => x * y);
+							break;
+						}
+					case 99:
+						run = false;
+						break;
+					default:
+						throw new InvalidOperationException();
+				}
+
+				index += 4;
+			}
+		}
+
+		private void Math(int address, Func<int, int, int> func)
+		{
+			int index1 = _memory[address + 1];
+			int index2 = _memory[address + 2];
+			int destIndex = _memory[address + 3];
+
+			_memory[destIndex] = func(_memory[index1], _memory[index2]);
+		}
+	}
+
+	public enum AddressType
+	{
+		Unknown,
+		OpCode,
+		Variable,
+	}
+
+	public class Memory
+	{
+		private readonly List<int> _memory;
+		private readonly AddressType[] _memoryType;
+
+		public Memory(List<int> data)
+		{
+			_memory = data;
+			_memoryType = new AddressType[data.Count];
+		}
+
+		public List<int> ReadAll()
+		{
+			return new List<int>(_memory);
+		}
+
+		public int this[int address]
+		{
+			get { return _memory[address]; }
+			set
+			{
+				_memory[address] = value;
+				Set(address, AddressType.Variable);
+			}
+		}
+
+		public void Set(int address, AddressType variable)
+		{
+			if (_memoryType[address] != variable && _memoryType[address] != AddressType.Unknown)
+			{
+				if (_memoryType[address] == AddressType.OpCode && variable == AddressType.Variable)
+				{ }
+				else
+				{
+					Console.WriteLine($"Opcode written to {address}");
+				}
+			}
+
+			_memoryType[address] = variable;
+		}
+	}
+}
