3c58ff4a55fe0f7bfc8747ecc9c432862cc639b9
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2019-12-15 00:39:49 -0500

    Day 15 scaffold

---
+++ 15/Day15/Day15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RepairBot bot = new RepairBot();
+
+            while (true)
+            {
+                bot.Run();
+            }
+        }
+    }
+}
