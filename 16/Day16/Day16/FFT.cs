cc65629814890e63037888714f137d95a12583bc
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2019-12-16 21:58:15 -0500

    Day 16 part 1

---
+++ 16/Day16/Day16/FFT.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day16
+{
+    internal class FFT
+    {
+        static readonly int[] _basePattern = new[] { 0, 1, 0, -1 };
+
+        private readonly int[] _input;
+
+        public int[] Input => _input;
+
+        public FFT(int[] input)
+        {
+            _input = input;
+        }
+
+        public int[] RunPhase()
+        {
+            return Phase(_input, _basePattern).ToArray();
+        }
+
+        private IEnumerable<int> Phase(int[] input, int[] basePattern)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                int[] multipliedPattern = GetMultiplier(basePattern, i, input.Length).ToArray();
+
+                int sum = 0;
+                for (int k = 0; k < input.Length; k++)
+                {
+                    sum += input[k] * multipliedPattern[k + 1];
+                }
+
+                var result = Math.Abs(sum) % 10;
+
+                yield return result;
+            }
+        }
+
+        private IEnumerable<int> GetMultiplier(int[] basePattern, int index, int inputLength)
+        {
+            inputLength++;
+            while(inputLength >= 0)
+            {
+                for (int i = 0; i < basePattern.Length; i++)
+                {
+                    for (int k = 0; k < index + 1; k++)
+                    {
+                        inputLength--;
+                        yield return basePattern[i];
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 16/Day16/Day16/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringRunFFT(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 4));
+
+            Debug.Assert(StringRunFFT("80871224585914546619083218645595", 100) == "24176176");
+            Debug.Assert(StringRunFFT("19617804207202209144916044189917", 100) == "73745418");
+            Debug.Assert(StringRunFFT("69317163492948606335995924319873", 100) == "52432133");
+
+            Console.WriteLine(StringRunFFT(Input, 100));
+
+        }
+
+        private static string StringRunFFT(string input, int iterations)
+        {
+            return StringRunFFT(input.Select(x => int.Parse(x.ToString())).ToArray(), iterations);
+        }
+
+        private static string StringRunFFT(int[] input, int iterations)
+        {
+            return string.Join("", RunFFT(input, iterations).Take(8));
+        }
+
+        private static int[] RunFFT(int[] input, int iterations)
+        {
+            FFT fft = new FFT(input);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var next = fft.RunPhase();
+
+                Console.WriteLine("Phase {0}:\t{1}", i + 1, string.Join(", ", next));
+
+                fft = new FFT(next);
+            }
+
+            return fft.Input;
+        }
+
+        private static string Input = "59791911701697178620772166487621926539855976237879300869872931303532122404711706813176657053802481833015214226705058704017099411284046473395211022546662450403964137283487707691563442026697656820695854453826690487611172860358286255850668069507687936410599520475680695180527327076479119764897119494161366645257480353063266653306023935874821274026377407051958316291995144593624792755553923648392169597897222058613725620920233283869036501950753970029182181770358827133737490530431859833065926816798051237510954742209939957376506364926219879150524606056996572743773912030397695613203835011524677640044237824961662635530619875905369208905866913334027160178";
+    }
+}
